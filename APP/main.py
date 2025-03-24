31444198b5d4985de6e14359b278921c1cff89f9
Sanjay s <94231938+Sanjay-shankar-ai@users.noreply.github.com>
2025-03-24 11:52:27 +0530

    Update main.py

--- APP/main.py
+++ APP/main.py
@@ -1,17 +1,43 @@
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
-
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+def send_email(sender_email, receiver_email, subject, body, smtp_server, smtp_port, sender_password):
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
+            st.success(f"✅ Email sent successfully to {receiver_email}")
+    except Exception as e:
+        st.error(f"❌ Failed to send email: {e}")
+
 
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("📧 Cold Mail Generator")
+
     url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-33460")
+    sender_email = st.text_input("Your Email", value="sanhulk73@gmail.com")
+    receiver_email = st.text_input("Receiver's Email", value="sanjayshankar91@gmail.com")
+    sender_password = st.text_input("Your Email Password", type="password")
+    
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+
     submit_button = st.button("Submit")
 
     if submit_button:
@@ -20,11 +46,15 @@ def create_streamlit_app(llm, portfolio, clean_text):
             data = clean_text(loader.load().pop().page_content)
             portfolio.load_portfolio()
             jobs = llm.extract_jobs(data)
+
             for job in jobs:
                 skills = job.get('skills', [])
                 links = portfolio.query_links(skills)
-                email = llm.write_mail(job, links)
-                st.code(email, language='markdown')
+                email_body = llm.write_mail(job, links)
+
+                subject = "Expert Solutions for Your Job Requirement"
+                send_email(sender_email, receiver_email, subject, email_body, smtp_server, smtp_port, sender_password)
+
         except Exception as e:
             st.error(f"An Error Occurred: {e}")
 
@@ -34,5 +64,3 @@ if __name__ == "__main__":
     portfolio = Portfolio()
     st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
     create_streamlit_app(chain, portfolio, clean_text)
-
-
